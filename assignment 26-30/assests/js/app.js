93a9089706f15de481aab8607a2396b20692ad63
siddiquesahar123 <126084762+siddiquesahar123@users.noreply.github.com>
2023-06-23 13:11:05 -0700

    Add files via upload

---
+++ assignment 26-30/assests/js/app.js
@@ -0,0 +1,100 @@
+// 1. Write a program that takes a positive integer from user &
+// display the following in your browser.
+
+// a. number
+// let integer = parseInt(prompt("Enter the positive integar:"));
+// document.write("Number: " + integer);
+
+// b. round off value of the number
+// let round = Math.round(integer);
+// document.write("Round off: " + round);
+
+// // c. floor value of the number
+// let floor = Math.floor(integer);
+// document.write("Floor value: " + floor);
+
+// // d. ceil value of the number
+// let ceil = Math.ceil(integer);
+// document.write("Ceil value: " + ceil);
+
+// 2. Write a program that takes a negative floating point
+// number from user & display the following in your browser.
+
+// a. number
+// let integer = parseFloat(prompt("Enter the negative integar:"));
+// document.write("Number: " + integer);
+
+// // b. round off value of the number
+// let round = Math.round(integer);
+// document.write("Round off: " + round);
+
+// // c. floor value of the number
+// let floor = Math.floor(integer);
+// document.write("Floor value: " + floor);
+
+// // d. ceil value of the number
+// let ceil = Math.ceil(integer);
+// document.write("Ceil value: " + ceil);
+
+// 3. Write a program that displays the absolute value of a
+// number.
+// E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+// let number = parseInt(prompt("Enter the number:"));
+// let absoluteValue = Math.abs(number);
+// document.write("The absolute value of " + number + " " + "is " + absoluteValue);
+
+// 4. Write a program that simulates a dice using random()
+// method of JS Math class. Display the value of dice in your
+// browser.:
+// Simulate a dice roll
+
+// let diceValue = Math.floor(Math.random() * 6) + 1;
+// document.write("Random dice value is: " + diceValue);
+
+// 5. Write a program that simulates a coin toss using random()
+// method of JS Math class. Display the value of coin in your
+// browser
+
+// let num = Math.random();
+// if (num < 0.5) {
+//     document.write("Random coin value is: Head");
+// }
+// else {
+//     document.write("Random coin value is: Tails");
+// } 
+
+// 6. Write a program that shows a random number between 1
+// and 100 in your browser.
+// generating  a random number
+// generating a random number
+// let number = Math.floor(Math.random() * 100) + 1;
+// document.write("Random number between 1 and 100: " + number);
+
+// 7. Write a program that asks the user about his weight. Parse
+// the user input and display his weight in your browser.
+// Possible user inputs can be:
+// a. 50
+// b. 50kgs
+// c. 50.2kgs
+// d. 50.2kilograms
+
+// let weight = prompt("Enter the weight in kilograms: ");
+// let input = parseFloat(weight);
+// document.write("The weight of user is " + input + " " + " kgs");
+
+// 8. Write a program that stores a random secret number from
+// 1 to 10 in a variable. Ask the user to input a number
+// between 1 and 10. If the user input equals the secret
+// number, congratulate the user.
+
+// let number = Math.floor(Math.random() * 100) + 1;
+// let userInput = parseInt(prompt("Guess the secret number between (1 and 10):"));
+// if (userInput === number) {
+//   document.write("Congratulation! you guess the secret number:");
+// }
+// else{
+//     document.write("Sorry! " + number + " Try again!");
+// }
+
+
