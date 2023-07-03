2083e56f90e12d70c433ff1061fb15727fce5a08
siddiquesahar123 <126084762+siddiquesahar123@users.noreply.github.com>
2023-07-03 01:11:28 -0700

    Add files via upload

---
+++ assigment 1-2/assests/js/app.js
@@ -0,0 +1,70 @@
+// ASSIGMENT 1
+// 1. Write a script to greet your website visitor using JS alert box.
+// 2. Write a script to display following message on your web page:
+// alert("JavaScript Alerts \n \n \n Please! don't enter invalid password");
+
+// 3. Write a script to display following message on your web page:
+// alert("JavaScript Alerts \n \n \n Wecome to JS... \n Land Happy coding");
+
+// 4. Write a script to display following messages in sequence:
+// alert("JavaScript Alerts \n \n \n Welcome to JS Land");
+
+// alert("JavaScript Alerts \n \n \n Happy coding");
+
+// 5. Generate the following message through browser’s developer console:
+
+// alert("Javascript Alert \n \n \n Hello.. I can JS through my web browser console")
+
+// ASSIGMENT 2
+
+// Declare a variable called username
+// let username;
+
+//Declare a variable called myName & assign to it a string that represents your Full Name.
+// var myName = "Sahar Aziz";
+// console.log(myName);
+
+//  Write script to
+
+// a) Declare a JS variable, titled message.
+// var message;
+
+// b) Assign “Hello World” to variable message
+// var message = "Hello World";
+
+// c) Display the message in alert box.
+// alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// Save student's bio data in JS variables
+
+// var a = 15;
+// var b = "years old "
+// var c = a + b;
+// alert(a + b);
+
+// 5. Write a script to display the following alert using one JS variable:
+
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+// Declare a variable called email and assign a string representing your email address
+// let email = "example@example.com";
+
+// Display an alert box with a message that includes the email address using string concatenation
+// alert("My email address is " + email );
+
+// VARIABLES FOR STRINGS | JAVASCRIPT
+
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+
+// var book = "A smarter \n way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book); 
+
+// 8. Write a script to display this in browser through JS
+
+// var text = "Yaah! I cant write HTML content through JavaScript";
+// document.write(text);
+
+//9. Store following string in a variable and show in alert and browser through JS
+// var img = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(img);
