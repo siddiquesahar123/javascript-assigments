a0ec19f128d6a776554d756f3c38e482ff9800fb
siddiquesahar123 <126084762+siddiquesahar123@users.noreply.github.com>
2023-07-22 13:37:07 -0700

    assignment 49-52

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,55 +1,90 @@
-// 1. Show an alert box on click on a link.
-// function box() {
-//     alert('Hello world!');
+// ====Q:1====
+
+// let signupBtn = document.getElementById("signupBtn");
+// let signinBtn = document.getElementById("signinBtn");
+// let nameField = document.getElementById("nameField");
+// let title = document.getElementById("title");
+
+// signinBtn.onclick = function(){
+//     nameField.style.maxHeight = "0";
+//     title.innerHTML = "Sign In";
+//     signupBtn.classList.add("disable");
+//     signinBtn.classList.remove("disable");
+// }
+// signupBtn.onclick = function(){
+//     nameField.style.maxHeight = "60px";
+//     title.innerHTML = "Sign Up";
+//     signupBtn.classList.remove("disable");
+//     signinBtn.classList.add("disable");
 // }
 
-// 2. Display some Mobile images in browser. On click on an
-// image Show the message in alert to user.
+// ====Q:2======
+// let readMore = document.querySelector('.read-more-btn');
+// let text = document.querySelector('.text');
 
-// function showAlt(altTxt){
-//     alert ("You click on ", altTxt);
+// readMore.addEventListener('click',(e)=>{
+//     text.classList.toggle('show-more');
+//     if(readMore.innerText === 'Read More'){
+//         readMore.innerText = 'Read less';
+//     }
+//     else{
+//         readMore.innerText = 'Read More';
+//     }
+// })
+
+// =====Q:3=====
+
+// let studentForm = document.getElementById('studentForm');
+// let tableBody = document.getElementById('tableBody');
+// let editedRowIndex = null;
+// studentForm.addEventListener('submit',function(event){
+//   event.preventDefault();
+
+//   let name = document.getElementById('name').value;
+//   let age = document.getElementById('age').value;
+//   let email = document.getElementById('email').value;
+
+//   if (name && age && email){
+//     if(editedRowIndex !== null){
+//       updateRow(editedRowIndex, name, age, email);
+//       editedRowIndex = null;
+//     }
+//     else{
+//       addRow(name, age, email);
+//     }
+//       studentForm.reset();
+//     }
+//     else{
+//       alert("'Please fill in all fields.")
+//   }
+// })
+// function addRow(name, age, email){
+//   let row = document.createElement('tr');
+//   row.innerHTML = `
+//   <td>${name}</td>
+//     <td>${age}</td>
+//     <td>${email}</td>
+//     <td>
+//     <button type="button" onclick="editRow(this)">Edit</button>
+//     <button type="button" onclick="deleteRow(this)">Delete</button>
+//   </td>
+// `;
+// tableBody.appendChild(row);
 // }
-
-// 3. Display 10 student records in table and each row should contain a delete
-// button. If you click on a button to delete a record, entire row should be
-// deleted.
-
 // function deleteRow(button){
-//     let row = button.parentNode.parentNode;
-//     row.parentNode.removeChild(row);
+//   let row = button.parentNode.parentNode;
+//   tableBody.removeChild(row)
+// }
+// function editRow(button){
+//   document.getElementById('name').value = name;
+//   document.getElementById('age').value = age;
+//   document.getElementById('email').value = email;
+//   editedRowIndex = row.rowIndex;
 // }
 
-// 4. Display an image in browser. Change the picture on mouseover and set the
-// first picture on mouseout.
-
-// function setNewImage() {
-//     document.getElementById('img1').src = "./assests/images/photo-2.avif";
+// function updateRow(rowIndex, name, age, email) {
+//   let row = tableBody.rows[rowIndex - 1];
+//   row.cells[0].innerText = name;
+//   row.cells[1].innerText = age;
+//   row.cells[2].innerText = email;
 // }
-// function setOldImage() {
-//     document.getElementById('img1').src = "./assests/images/photo-1.avif";
-// }
-
-// 5. Show a counter in browser. Counter should increase on click on increase
-// button and decrease on click on decrease button. And show updated counter
-// value in browser.
-
-// let plus = document.querySelector(".plus"),
-// minus = document.querySelector(".minus"),
-// num = document.querySelector(".num");
-
-// let a = 1;
-
-// plus.addEventListener("click", ()=>{
-//     a++;
-//     a = (a < 10) ? "0" + a : a;
-//     num.innerText = a;
-//     console.log(a);
-// })
-
-// minus.addEventListener("click", ()=>{
-//    if (a > 1) {
-//     a--;
-//     a = (a < 10) ? "0" + a : a;
-//     num.innerText = a;
-//    }
-// })
